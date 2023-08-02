3f1f35d0db2555b6ae17291243ceb1d6187feaaa
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-02 15:37:59 +0400

    верстка компонента aboutProject, добавил шрифты

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import Promo from "./Promo";
+import AboutProject from "./AboutProject";
 
 function Main () {
     return (
         <main className="main">
             <Promo />
+            <AboutProject />
         </main>
     )
 }
