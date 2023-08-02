a3a53aaa995af67148e45782e1e76c69ddd580ed
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-02 19:59:06 +0400

    верстка компонента techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
-import Promo from "./Promo";
-import AboutProject from "./AboutProject";
+import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProjects/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main () {
     return (
         <main className="main">
             <Promo />
             <AboutProject />
+            <Techs />
         </main>
     )
 }
---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,24 @@
+function Techs () {
+    return (
+        <section className="techs">
+            <h2 className="techs__title">Технологии</h2>
+            <div className="techs__container">
+                <h3 className="techs__container_title">7 технологий</h3>
+                <p className="techs__container_subtitle">На курсе веб-разработки мы освоили технологии,
+                    которые применили в дипломном проекте.
+                </p>
+            </div>
+            <ul className="techs__list">
+                <li className="techs__list_item">HTML</li>
+                <li className="techs__list_item">CSS</li>
+                <li className="techs__list_item">JS</li>
+                <li className="techs__list_item">React</li>
+                <li className="techs__list_item">Git</li>
+                <li className="techs__list_item">Express.js</li>
+                <li className="techs__list_item">mongoDB</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Techs;
