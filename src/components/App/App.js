d643b4831974e8a28ffe252adff2def69eecfd84
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-03 18:54:33 +0400

    верстка компонента footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+function Footer () {
+    return (
+        <footer className="footer">
+            <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+            <div className="footer__container">
+                <p className="footer__container_year">© 2023</p>
+                <div className="footer__container_links">
+                    <a className="footer__container_link" href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
+                    <a className="footer__container_link" href="https://github.com/Moscow89er/">Github</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
