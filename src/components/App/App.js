3466119a129af7aaf692d224bfddffb375a74d0f
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-03 22:48:10 +0400

    работа над адаптивом

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Movies from '../Movies/Movies';
 import Footer from "../Footer/Footer";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
