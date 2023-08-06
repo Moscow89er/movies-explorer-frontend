1222fdf0a2fb2f52a9ede2ad396b608007e904de
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-06 15:49:50 +0400

    работа над адаптивом 8

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,10 +1,27 @@
+import React, { useState, useEffect } from "react";
 import { movies } from "../../utils/data";
 
 function MoviesCard () {
+    const [windowDimensions, setWindowDimensions] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWindowDimensions(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    let itemsToShow;
+
+    if (windowDimensions < 920) {
+        itemsToShow = 8;
+    } else {
+        itemsToShow = 16;
+    }
+
     return (
         <section className="movies-card">
             <div className="movies-card__container_items">
-                {movies.map((movie) => (
+                {movies.slice(0, itemsToShow).map((movie) => (
                     <div className="movies-card__container_item" key={movie.id}>
                         <img 
                             src={movie.pic}
@@ -22,7 +39,7 @@ function MoviesCard () {
                     </div>
                 ))}
             </div>
-            <button type="submit" className="movies-card_button">Ещё</button>
+            <button type="button" className="movies-card_button">Ещё</button>
         </section>
     )
 }
