8cc19ccfbe32e1decf22875a2b0fb505823cf981
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-06 11:35:27 +0400

    верстка компонента moviesCard 2

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,23 +1,26 @@
-import movieCardPic from "../../images/movie_card_pic.png"
+import { movies } from "../../utils/data";
 
 function MoviesCard () {
     return (
         <section className="movies-card">
-            <div className="movies-card__list">
-                <div className="movies-card__container">
-                    <img 
-                        src={movieCardPic}
-                        className="movies-card__pic"
-                        alt="Изображение фильма"
-                    />
-                    <div className="movies-card__container">
-                        <h3 className="movies-card__title">33 слова о дизайне</h3>
-                        <label className="movies-card_radio">
-                            <input type="radio" className="movies-card_radio_input"/>
-                            <div className="movies-card_radio_circle" />
-                        </label>
+            <div className="movies-card__container_items">
+                {movies.map((movie) => (
+                    <div className="movies-card__container_item">
+                        <img 
+                            src={movie.pic}
+                            className="movies-card__pic"
+                            alt="Изображение фильма"
+                        />
+                        <div className="movies-card__container_radio">
+                            <h3 className="movies-card__title">{movie.title}</h3>
+                            <label className="movies-card_radio">
+                                <input type="radio" className="movies-card_radio_input"/>
+                                <div className="movies-card_radio_circle" />
+                            </label>
+                        </div>
+                        <p className="movies-card__time">{movie.time}</p>
                     </div>
-                </div>
+                ))}
             </div>
         </section>
     )
