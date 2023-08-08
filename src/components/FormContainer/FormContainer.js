58daa27404d31fc791620f591c3f1c81a555ce9d
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-08 22:48:41 +0400

    верстка компонента formContainer 2

--- src/components/FormContainer/FormContainer.js
+++ src/components/FormContainer/FormContainer.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
-import formContainerLogo from "../../images/blue_logo.svg"
+import React from "react";
+import formContainerLogo from "../../images/blue_logo.svg";
+import useFormValidator from "../../utils/useFormValidator";
 
 
 function FormContainer ({ titleText, buttonText, paragraphText, parentComponent }) {
-    const [valueName, setValueName] = useState("Виталий");
-    const [valueEmail, setValueEmail] = useState("pochta@yandex.ru");
-    const [valuePassword, setValuePassword] = useState("");
-
-    const handleInputChange = (evt) => {
-        const { name, value } = evt.target;
-        if (name === 'name') {
-            setValueName(value);
-        } else if (name === 'email') {
-            setValueEmail(value);
-        } else if (name === 'password') {
-            setValuePassword(value);
-        }
+    const initialState = {
+        name: "Виталий",
+        email: "pochta@yandex.ru",
+        password: ""
     }
+
+    const { formValues, formErrors, handleInputChange } = useFormValidator(initialState);
 
     return (
         <section className="form-container">
@@ -27,41 +21,52 @@ function FormContainer ({ titleText, buttonText, paragraphText, parentComponent 
                     {parentComponent === "Register" &&
                     <>
                         <p className="form-container__paragraph">Имя</p>
-                        <input
-                            type="text"
-                            name="name"
-                            value={valueName}
-                            onChange={handleInputChange}
-                            className="form-container__input"
-                        />
-                        <span className="form-container__error"></span>
+                        <div className="form-container__input_wrapper">
+                            <input
+                                type="text"
+                                name="name"
+                                value={formValues.name}
+                                onChange={handleInputChange}
+                                className={`form-container__input ${formErrors.name ? "form-container__input_error" : ""}`}
+                            />
+                            <span className="form-container__error">{formErrors.name}</span>
+                        </div>
                     </>
                     }
                     <p className="form-container__paragraph">E-mail</p>
-                    <input
-                        type="email"
-                        name="email"
-                        value={valueEmail}
-                        onChange={handleInputChange}
-                        className="form-container__input"
-                    />
-                    <span className="form-container__error"></span>
+                    <div className="form-container__input_wrapper">
+                        <input
+                            type="email"
+                            name="email"
+                            value={formValues.email}
+                            onChange={handleInputChange}
+                            className={`form-container__input ${formErrors.email ? "form-container__input_error" : ""}`}
+                        />
+                        <span className="form-container__error">{formErrors.email}</span>
+                    </div>
                     <p className="form-container__paragraph">Пароль</p>
-                    <input
-                        type="password"
-                        name="password"
-                        value={valuePassword}
-                        onChange={handleInputChange}
-                        className="form-container__input"
-                    />
-                    <span className="form-container__error">Что-то пошло не так...</span>
-                    <button className="form-container__button_submit">{buttonText}</button>
+                    <div className="form-container__input_wrapper">
+                        <input
+                            type="password"
+                            name="password"
+                            value={formValues.password}
+                            onChange={handleInputChange}
+                            className={`form-container__input ${formErrors.password ? "form-container__input_error" : ""}`}
+                            minLength={4}
+                            maxLength={16}
+                        />
+                        <span className="form-container__error">{formErrors.password}</span>
+                    </div>
+                    {parentComponent === "Register"
+                        ? <button className="form-container__button_submit">{buttonText}</button>
+                        : <button style={{margin: "158px auto 0"}} className="form-container__button_submit">{buttonText}</button>
+                    }
                 </form>
                 <div className="form-container__links">
                     <p className="form-container__links_paragraph">{paragraphText}</p>
                     {parentComponent === "Register"
-                        ? <a className="form-container__link">Войти</a>
-                        : <a className="form-container__link">Регистрация</a>
+                        ? <a href="#" className="form-container__link">Войти</a>
+                        : <a href="#" className="form-container__link">Регистрация</a>
                     }
                 </div>
             </div>
