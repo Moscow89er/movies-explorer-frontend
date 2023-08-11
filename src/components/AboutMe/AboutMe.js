60b9f5b150517da5ea4bb51435baaa9826961361
Nikita Khubaev <khubaev.n@yandex.ru>
2023-08-11 22:17:34 +0400

    рефакторинг компонента aboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,16 +5,16 @@ function AboutMe () {
         <section className="about-me">
             <h2 className="about-me__title">Студент</h2>
             <div className="about-me__container">
-                <div className="about-me__container-text">
-                    <h3 className="about-me__container-text_title">Виталий</h3>
-                    <p className="about-me__container-text_subtitle">Фронтенд-разработчик, 30 лет</p>
-                    <p className="about-me__container-text_paragraph">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+                <div className="about-me__text">
+                    <h3 className="about-me__name">Виталий</h3>
+                    <p className="about-me__profession">Фронтенд-разработчик, 30 лет</p>
+                    <p className="about-me__info">Я родился и живу в Саратове, закончил факультет экономики СГУ.
                         У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
                         С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
                         начал заниматься фриланс-заказами и ушёл с постоянной работы.
                     </p>
                     <a 
-                        className="about-me__container-text_link"
+                        className="about-me__link"
                         href="https://github.com/Moscow89er/"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -22,7 +22,7 @@ function AboutMe () {
                         Github
                     </a>
                 </div>
-                <img className="about-me__container_image" src={aboutMePhoto} alt="Фотография студента" />
+                <img className="about-me__image" src={aboutMePhoto} alt="Фотография студента" />
             </div>
         </section>
     )
